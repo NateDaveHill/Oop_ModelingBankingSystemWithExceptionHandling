979a345db1fa56a94fb5a6d2440335dc4f34060a
NateDaveHill <nathanhill123@gmail.com>
2024-01-26 17:28:19 -0500

    testing functionality in main Program.cs

--- BankingModel/Program.cs
+++ BankingModel/Program.cs
@@ -1,3 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+using System.IO.Compression;
+using BankingModel;
 
-Console.WriteLine("Hello, World!");
+Bank newBank = new Bank("HillTestingBank");
+BankAccount newBankAccountOne = new BankAccount(12345, "Nathan");
+BankAccount newBankAccountTwo = new BankAccount(67890, "Staree");
+
+
+newBank.addAccount(newBankAccountOne);
+newBank.addAccount(newBankAccountTwo);
+
+
+
+
+newBank.getBankName();
+newBank.getAccountByNumber(12345);
+newBank.getAccountByNumber(67890);
+newBank.transferMoney(12345, 67890, 10);
+newBank.getReport(newBankAccountOne);
+newBank.getReport(newBankAccountTwo);
--- BankingModel/bank/Bank.cs
+++ BankingModel/bank/Bank.cs
@@ -3,13 +3,17 @@ namespace BankingModel;
 public class Bank
 {
     public string BankName { get; set; }
-    public List<BankAccount> Accounts { get; set; }
+    public List<BankAccount>? Accounts { get; set; }
 
-    public Bank(string bankName, List<BankAccount> accounts)
+    public Bank(string bankName, List<BankAccount>? accounts)
     {
         this.BankName = bankName;
         this.Accounts = accounts;
 
+    }
+    public Bank(string bankName)
+    {
+        this.BankName = bankName;
     }
 
     public void addAccount(BankAccount account)
