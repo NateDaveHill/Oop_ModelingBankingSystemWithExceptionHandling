50b0731c9d87e37c185621d40f414ddabdb0fb7f
NateDaveHill <nathanhill123@gmail.com>
2024-01-26 17:08:29 -0500

    Finished up the Bank calss with more methods and functionality

--- BankingModel/bank/Bank.cs
+++ BankingModel/bank/Bank.cs
@@ -11,4 +11,45 @@ public class Bank
         this.Accounts = accounts;
 
     }
+
+    public void addAccount(BankAccount account)
+    {
+        Accounts.Add(account);
+    }
+
+    public BankAccount getAccountByNumber(int accountNumber)
+    {
+        foreach (var i in Accounts)
+        {
+            if (i.AccountNumber == accountNumber)
+            {
+                return i;
+            }
+        }
+
+        return null;
+    }
+
+    public void transferMoney(int fromAccountNumber, int toAccountNumber, double amount)
+    {
+        BankAccount foundAccountSender = Accounts.Find(x => x.AccountNumber == fromAccountNumber);
+        BankAccount foundAccountReceiver = Accounts.Find(x => x.AccountNumber == toAccountNumber);
+        foundAccountSender.withdraw(amount);
+        foundAccountReceiver.deposit(amount);
+
+    }
+
+    public string getBankName()
+    {
+        return BankName;
+    }
+
+    public void getReport(BankAccount account)
+    {
+        Console.WriteLine("Account number: {0}", account.AccountNumber);
+        Console.WriteLine("Account Holder Name: {0}", account.AccountHolderName);
+        Console.WriteLine("Balance {0}", account.Balance);
+        Console.WriteLine("Bank Name: {0}", BankName);
+    }
+    
 }
