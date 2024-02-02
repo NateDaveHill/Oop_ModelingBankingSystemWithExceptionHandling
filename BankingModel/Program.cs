c0edc5c5ec42d5b3f9b430789af459fa9db7d1ea
NateDaveHill <nathanhill123@gmail.com>
2024-02-02 09:47:35 -0500

    testing functionality of the methods - passed

--- BankingModel/Program.cs
+++ BankingModel/Program.cs
@@ -10,19 +10,21 @@ BankAccount newBankAccountTwo = new BankAccount(67890, "Staree");
 newBank.addAccount(newBankAccountOne);
 newBank.addAccount(newBankAccountTwo);
 
-
-Console.WriteLine(newBankAccountOne.getAccountHolderName());
 newBankAccountOne.deposit(100);
-Console.WriteLine(newBankAccountOne.getBalance());
-Console.WriteLine(newBankAccountOne.getAccountNumber());
 
-newBankAccountOne.withdraw(5);
-Console.WriteLine(newBankAccountOne.getBalance());
+newBankAccountTwo.deposit(100);
 
+newBank.getReport(newBankAccountOne);
+newBank.getReport(newBankAccountTwo);
 
-// newBank.getBankName();
-// newBank.getAccountByNumber(12345);
-// newBank.getAccountByNumber(67890);
-// newBank.transferMoney(12345, 67890, 10);
 // newBank.getReport(newBankAccountOne);
 // newBank.getReport(newBankAccountTwo);
+
+// Console.WriteLine(newBank.getBankName());
+
+newBank.getBankName();
+newBank.getAccountByNumber(12345);
+newBank.getAccountByNumber(67890);
+newBank.transferMoney(12345, 67890, 10);
+newBank.getReport(newBankAccountOne);
+newBank.getReport(newBankAccountTwo);
